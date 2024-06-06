4b63f6cc8149cf5e13b8add7411878dc9af43599
MariaHusak <husakmaria74@gmail.com>
2024-06-06 10:47:17 +0300

    reload wsgi.py

--- mycalendar/wsgi.py
+++ mycalendar/wsgi.py
@@ -15,5 +15,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mycalendar.settings')
 
 application = get_wsgi_application()
-
+application = OpenTelemetryMiddleware(application)
 
