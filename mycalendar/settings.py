29af08ed16d693e14e877902cab8fb5841bdf39a
MariaHusak <husakmaria74@gmail.com>
2024-06-06 17:50:48 +0300

    add env variables

--- mycalendar/settings.py
+++ mycalendar/settings.py
@@ -29,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#^a095n96qp0demcyty4*=57!p5owx#1s8+dy+ix5yi05#er16'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 import logging
 
